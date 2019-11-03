0f1146371ca735508465080de05381931472025d
jennakohls <jennakohls@gmail.com>
2019-11-03 16:52:00 -0500

    snapping

--- load.js
+++ load.js
@@ -11,8 +11,8 @@ let Application = PIXI.Application,
       
       
 let app = new Application({ 
-    width: 375,         // default: 800
-    height: 667,        // default: 600
+    width: 384,         // default: 800
+    height: 700,        // default: 600
     antialias: true,    // default: false
     transparent: false, // default: false
     resolution: 1       // default: 1
@@ -29,115 +29,53 @@ let app = new Application({
 //Add pixi canvas to html document
 document.body.appendChild(app.view);
       
-      
 //load an image and run the `setup` function when it's done
 loader
   .add("rss/tileset.json")
   .load(setup);
 
-let a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,blank;
-//tiles = new Array(27);
-tiles = new Array();
-      
+//let a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,blank;
+let letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','_blank'];
+let pileSize = 4;
+let boardSize = 8; //square
+let pile = new PIXI.Container();
+let board = new PIXI.Container();
+let tileSize = 64;
 //This `setup` function will run when the image has loaded
-function setup() {
-
-//this is horrible. eventually i'll make this more efficient.    
-a = new Sprite(TextureCache["a.png"]);
-    tiles.push(new Sprite(TextureCache["a.png"]));
-    app.stage.addChild(a);
-b = new Sprite(TextureCache["b.png"]);
-    tiles.push(new Sprite(TextureCache["b.png"]));
-    app.stage.addChild(b);
-c = new Sprite(TextureCache["c.png"]);
-    tiles.push(new Sprite(TextureCache["c.png"]));
-    app.stage.addChild(c);
-d = new Sprite(TextureCache["d.png"]);
-    tiles.push(new Sprite(TextureCache["d.png"]));
-    app.stage.addChild(d);
-e = new Sprite(TextureCache["e.png"]);
-    tiles.push(new Sprite(TextureCache["e.png"]));
-    app.stage.addChild(e);
-f = new Sprite(TextureCache["f.png"]);
-    tiles.push(new Sprite(TextureCache["f.png"]));
-    app.stage.addChild(f);    
-g = new Sprite(TextureCache["g.png"]);
-    tiles.push(new Sprite(TextureCache["g.png"]));
-    app.stage.addChild(g);   
-h = new Sprite(TextureCache["h.png"]);
-    tiles.push(new Sprite(TextureCache["h.png"]));
-    app.stage.addChild(h);
-i = new Sprite(TextureCache["i.png"]);
-    tiles.push(new Sprite(TextureCache["i.png"]));
-    app.stage.addChild(i);
-j = new Sprite(TextureCache["j.png"]);
-    tiles.push(new Sprite(TextureCache["j.png"]));
-    app.stage.addChild(j);
-k = new Sprite(TextureCache["k.png"]);
-    tiles.push(new Sprite(TextureCache["k.png"]));
-    app.stage.addChild(k);
-l = new Sprite(TextureCache["l.png"]);
-    tiles.push(new Sprite(TextureCache["l.png"]));
-    app.stage.addChild(l);
-m = new Sprite(TextureCache["m.png"]);
-    tiles.push(new Sprite(TextureCache["m.png"]));
-    app.stage.addChild(m);
-n = new Sprite(TextureCache["n.png"]);
-    tiles.push(new Sprite(TextureCache["n.png"]));
-    app.stage.addChild(n);
-o = new Sprite(TextureCache["o.png"]);
-    tiles.push(new Sprite(TextureCache["o.png"]));
-    app.stage.addChild(o);
-p = new Sprite(TextureCache["p.png"]);
-    tiles.push(new Sprite(TextureCache["p.png"]));
-    app.stage.addChild(p);
-q = new Sprite(TextureCache["q.png"]);
-    tiles.push(new Sprite(TextureCache["q.png"]));
-    app.stage.addChild(q);
-r = new Sprite(TextureCache["r.png"]);
-    tiles.push(new Sprite(TextureCache["r.png"]));
-    app.stage.addChild(r);
-s = new Sprite(TextureCache["s.png"]);
-    tiles.push(new Sprite(TextureCache["s.png"]));
-    app.stage.addChild(s);
-t = new Sprite(TextureCache["t.png"]);
-    tiles.push(new Sprite(TextureCache["t.png"]));
-    app.stage.addChild(t);
-u = new Sprite(TextureCache["u.png"]);
-    tiles.push(new Sprite(TextureCache["u.png"]));
-    app.stage.addChild(u);
-v = new Sprite(TextureCache["v.png"]);
-    tiles.push(new Sprite(TextureCache["v.png"]));
-    app.stage.addChild(v);
-w = new Sprite(TextureCache["w.png"]);
-    tiles.push(new Sprite(TextureCache["w.png"]));
-    app.stage.addChild(w);
-x = new Sprite(TextureCache["x.png"]);
-    tiles.push(new Sprite(TextureCache["x.png"]));
-    app.stage.addChild(x);
-y = new Sprite(TextureCache["y.png"]);
-    tiles.push(new Sprite(TextureCache["y.png"]));
-    app.stage.addChild(y);
-z = new Sprite(TextureCache["z.png"]);
-    tiles.push(new Sprite(TextureCache["z.png"]));
-    app.stage.addChild(z);
-blank = new Sprite(TextureCache["_blank.png"]);
-    tiles.push(new Sprite(TextureCache["_blank.png"]));
-    app.stage.addChild(blank);
-
-let tileSz = 64;
-  //Render the stage   
-createA(32,64 + 32);
+function setup() { 
+    
+    //make board
+    for(let i = 0; i < boardSize; i++){
+        for(let j = 0; j < boardSize; j++){
+            let tileName = "_blank";
+            let newTile = createTile(tileName,(i * tileSize) + tileSize/2,(j * tileSize) + tileSize/2);
+            board.addChild(newTile);
+        }
+    }
+    app.stage.addChild(board);
+    //make pile
+    for(let i = 0; i < pileSize; i++){
+        let tileName = letters[Math.round(Math.random() * (letters.length - 1))];
+        let newPos = roundPosition((i * tileSize) + tileSize/2,tileSize/2);
+        let newTile = createTile(tileName,newPos[0],newPos[1]); 
+        pile.addChild(newTile);
+    }
+    
+    pile.position.set(tileSize/2, (tileSize/2) * 17);
+    app.stage.addChild(pile);
+    
     
 }
 
-function createA(x,y){
-    a.y=64;
-    a.interactive = true;
-    a.buttonMode = true;
-    a.anchor.set(0.5);
+function createTile(name,x,y){
+    let path = name + ".png"
+    let tile = new Sprite(TextureCache[path]);
     
-    a // events for drag start
+    tile.interactive = true;
+    tile.buttonMode = true;
+    tile.anchor.set(0.5);
+    
+    tile // events for drag start
     .on('mousedown', onDragStart)
     .on('touchstart', onDragStart)
     // events for drag end
@@ -150,8 +88,12 @@ function createA(x,y){
     .on('touchmove', onDragMove);
 
     // move the sprite to its designated position
-    a.position.x = x;
-    a.position.y = y;
+    tile.position.x = x;
+    tile.position.y = y;
+    
+    //app.stage.addChild(tile);
+    
+    return tile;
 }
 
 requestAnimationFrame( animate );
@@ -161,7 +103,7 @@ function animate() {
     requestAnimationFrame(animate);
 
     // render the stage
-    renderer.render(stage);
+    app.render(app.stage);
 }
 
 function onDragStart(event)
@@ -189,11 +131,24 @@ function onDragMove()
     if (this.dragging)
     {
         var newPosition = this.data.getLocalPosition(this.parent);
-        this.position.x = roundPosition(newPosition.x);
-        this.position.y = roundPosition(newPosition.y);
+        let newRounded = roundPosition(newPosition.x,newPosition.y);
+        this.position.set(newRounded[0],newRounded[1]);
     }
 }
 
-function roundPosition(x){
-    return Math.round(x/10)*10;
+function roundPosition(x,y){
+    //lol this doesn't work quite yet but... one day. it rounds, just doesn't bounce off the edge like i'd like
+    let amt = 64;
+    let roundedX = Math.round(x/amt)*amt;
+    let roundedY = Math.round(y/amt)*amt;
+    if(app.view.width - roundedX <= amt) {
+        roundedX -= amt;
+    }
+    if(app.view.height - roundedY <= amt) {
+        roundedY -= amt;
+    }
+    return [roundedX, roundedY];
+}
+function boxesCollide(a,b){
+    return ab.x + ab.width > bb.x && ab.x < bb.x + bb.width && ab.y + ab.height > bb.y && ab.y < bb.y + bb.height;
 }
